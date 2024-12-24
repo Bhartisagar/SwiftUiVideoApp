1b96f05c6983588568cc1360ea4da2819e45ee91
Bhartisagar <53816596+Bhartisagar@users.noreply.github.com>
2024-12-24 19:21:11 +0530

    added project

---
+++ SwiftUI-Sample/SwiftUI-SampleTests/SwiftUI_SampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleTests.swift
+//  SwiftUI-SampleTests
+//
+//  Created by Bharti Sagar on 24/12/24.
+//
+
+import Testing
+@testable import SwiftUI_Sample
+
+struct SwiftUI_SampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
